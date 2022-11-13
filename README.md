e82b5f58bbf97cbc449cd701bd4b743c2acd54e6
Visn0 <anton_chernysh@outlook.es>
2022-11-13 12:12:02 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# jump2digital2022
+# Jump2Digital 2022
 NUWE DataScience Hackathon
+
+## Background
+
+The Paris Agreement is an international treaty on climate change that was adopted by 196 Parties at COP21 in Paris. Their goal is to limit global warming below 2, preferably 1.5 degrees Celsius, compared to pre-industrial levels.
+
+In order to reach the goal, countries are trying to maximize their greenhouse gas emissions and archive a climate-neutral planet.
+
+That is why EU is investing in the development of new technologies that allow the improvement of the fight againts pollution. One of these is a new type of sensor based on laser technology that allows measure of air quality.
+
+## Problem
+
+The goal of the challenge is to develop and implement and predictive model based on *RandomForest* algorithms that allows to know the air quality using measurements of the sensors.
+
+To perform this task, we are provided with a training dataset, a dataset to predict, and an example of the expected format.
+
+## Results
+## Analysis
+## Solution
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ app.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+
+from os.path import join
+import os
+import sys
+
+DATA_PATH = 'data/'
+TRAIN_PATH= os.path.join(DATA_PATH, 'train.csv')
+TEST_PATH = os.path.join(DATA_PATH, 'test.csv')
+
+
+def change_column_names(df, df_test):
+    old_cols  = list(df.columns)
+    feat_cols = ['f'+str(x) for x in range(len(old_cols) - 1)]
+    new_cols  = feat_cols + old_cols[-1:]
+    target    = 'target'
+
+    df.columns = new_cols
+    df_test.columns = feat_cols
+
+    return df, df_test, feat_cols, target
+
+def load_data():
+    df      = pd.read_csv(TRAIN_PATH, sep=';')
+    df_test = pd.read_csv(TEST_PATH, sep=';')
+    print(df.shape, "raw shape df train")
+    print(df_test.shape, "raw shape df test")
+    return df, df_test
+
+def train_model(df, feat_cols, target):
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score, f1_score
+    from sklearn.metrics import make_scorer
+
+    X_train, X_val, y_train, y_val = train_test_split(df[feat_cols], df[target], test_size=0.2, random_state=42)
+    f1 = make_scorer(f1_score , average='macro')
+
+    param_grid = {
+        'n_estimators': [500, 800, 2000],
+        'max_depth' : [8, 10, 20, None],
+        'max_features': ['log2'],
+        'criterion' :['entropy'],
+        'class_weight' : [
+            None,
+            {0:0.25,1:0.25,2:0.5}, # target=2 more weight
+            {0:0.25,1:0.5,2:0.25}, # target=1 more weight
+            {0:0.2,1:0.4,2:0.4},   # target=1&2 more weight
+        ],
+    }
+    rf = RandomForestClassifier()
+    gs = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3, n_jobs=-1, scoring=f1)
+    gs.fit(X_train, y_train)
+    print("Best params:", gs.best_params_)
+    print("Best score:", gs.best_score_)
+
+    model = RandomForestClassifier(**gs.best_params_)
+
+    model.fit(X_train, np.ravel(y_train))
+    train_pred = model.predict(X_train)
+    val_pred   = model.predict(X_val)
+
+    train_acc = accuracy_score(y_train, train_pred)
+    val_acc   = accuracy_score(y_val, val_pred)
+    train_f1  = f1_score(y_train, train_pred, average='macro')
+    val_f1    = f1_score(y_val, val_pred, average='macro')
+
+    print("F1  (TRAIN | VAL): ", round(train_f1, 4), "|", round(val_f1, 4))
+    print("ACC (TRAIN | VAL): ", round(train_acc, 4), "|", round(val_acc, 4))
+
+    return model
+
+def main():
+    df, df_test = load_data()
+    df, df_test, feat_cols, target = change_column_names(df, df_test)
+
+    model = train_model(df, feat_cols, target)
+
+    # Create submission
+    test_pred = model.predict(df_test)
+    test_pred = pd.DataFrame(test_pred, columns=['final_status'])
+    test_pred.to_csv("predictions.csv", header=True, index=False)
+    test_pred.to_json("predictions.json", orient='columns')
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
